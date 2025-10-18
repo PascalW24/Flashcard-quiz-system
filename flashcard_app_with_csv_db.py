6d00d50ae90c4664bd6ead339aa64765adb60db2
Pascal Walther <pascal.walther@students.fhnw.ch>
2025-10-18 13:41:24 +0200

    Update flashcard_app_with_csv_db.py
    Implementation of the choosing of language

--- flashcard_app_with_csv_db.py
+++ flashcard_app_with_csv_db.py
@@ -1,4 +1,7 @@
 import csv
+import random
+
+
 
 def load_flashcards():
     flashcards = []
@@ -11,7 +14,34 @@ def load_flashcards():
         print(f"⚠️  File 'flashcard_words.csv' not found.")
     return flashcards
 
+
 def main():
     flashcards = load_flashcards()
+
+
+#Choose the language
+while True:
+    language = int(input("Enter 0 for German and 1 for English words: "))
+    if language == 0:
+        print("German")
+    elif language == 1:
+        print("English")
+    else:
+        print("Invalid input. Please try again.")
+
+# def
+
+
+# def
+
+
+# variables: amount_tries_word, amount_tries_overall, amount_right_words, variables for the words
+
+
+def quiz():
+    while(True):
+
+
+
 
 main()
